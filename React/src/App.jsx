42aaea7ec715eaba48a4882519243b480de360a6
Santoshpatel112 <your-correct-email@example.com>
2025-10-31 18:59:29 +0530

    Integrate Gallery component into main app

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,12 +3,14 @@ import Card from './Components/Card'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Hero from './Components/Hero'
+import Gallery from './Components/Gallery'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <Hero />
+      <Gallery />
       <Card />
       <Footer />
     </>
